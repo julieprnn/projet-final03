92d925cbcbe4bc9858d66a3dc1e67d4341ca7aee
Julie <juliepironin@gmail.com>
2021-04-28 13:28:12 +0200

    essai

---
+++ client/src/LogIn/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+export default function Login() {
+    const [email,setEmail] = useState("");
+    const [password,setPassWord] = useState("");
+
+    function validateForm() {
+        
+    }
+}
